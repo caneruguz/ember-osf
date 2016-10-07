52c77321dacb54c3bc4fedbf4772d46f4c5d4446
Caner Uguz <caneruguz@gmail.com>
2016-10-07 15:18:51 -0400

    Add multiselect function

--- addon/components/osf-tb-item/component.js
+++ addon/components/osf-tb-item/component.js
@@ -3,12 +3,19 @@ import layout from './template';
 
 export default Ember.Component.extend({
   layout,
-  selected : false,
-  click () {
-      this.toggleProperty('selected', true);
-      this.sendAction('clickItem', this.get('item'));
+  selected: Ember.computed('selectedItems.[]', function() {
+      // TODO: This would be better if selectedItems were a hash. Can Ember
+      // observe when properties are added to or removed from an object?
+      let selectedItems = this.get('selectedItems');
+      let index = selectedItems.indexOf(this.get('item'));
+      return index > -1;
+  }),
+  click (e) {
+      this.sendAction('multiSelect', this.get('item'), e);
   },
   doubleClick () {
-      this.sendAction('dblClickItem', this.get('item'));
+      if(this.get('item.kind') === 'folder'){
+          this.sendAction('dblClickItem', this.get('item'));
+      }
   }
 });
--- addon/components/osf-tb/component.js
+++ addon/components/osf-tb/component.js
@@ -10,34 +10,75 @@ export default Ember.Component.extend({
   files : Ember.A(),
   breadCrumbs : Ember.A(),
   columns: Ember.A(['itemName']),
+  selectedItems : Ember.A(),
   loadFiles : function (item) { // When node changes load node files
       this.set('files', Ember.A([]));
       if(item){
           loadAll(item, 'files', this.get('files'));
-          console.log(item, this.get('breadCrumbs'));
       } else {
           this.get('store').findRecord('node', this.get('nodeId')).then( model => {
               loadAll(model, 'files', this.get('files'));
           });
       }
   },
+  updateSelectedStatus : function(item){
+      let selected = this.get('selectedItems');
+      if(selected.includes(item)){
+          selected.removeObject(item);
+      } else {
+          selected.pushObject(item);
+      }
+  },
+
   actions : {
-      clickItem () {
-          console.log('clickItemTop');
-
-
+      clickItem (item) {
+          this.updateSelectedStatus(item);
       },
       dblClickItem (item) {
-          console.log('dblClickItemTop');
           this.loadFiles(item);
           this.breadCrumbs.pushObject(item);
-          console.log(this.get('breadCrumbs')[0]);
       },
       loadCrumb (item, index) {
-          console.log(item);
           this.loadFiles(item);
           let slicedCrumbs = this.breadCrumbs.slice(0, index + 1);
           this.set('breadCrumbs', Ember.A(slicedCrumbs));
+      },
+      multiSelect(item, e) {
+          let selected = this.get('selectedItems');
+          let items = this.get('files');
+          if(e.shiftKey || e.metaKey){
+              // handle multi select
+              if(e.metaKey){
+                  item.toggleProperty('isSelected');
+              }
+              if(e.shiftKey){
+                  // get the last item selected index
+                  let lastItem = selected[selected.length-1];
+                  let lastIndex = items.indexOf(lastItem);
+                  // get current item selected index
+                  var currentItemIndex = items.indexOf(item);
+                  // find out which index is bigger and buidl loop
+                  if(lastIndex > -1 && currentItemIndex > -1){
+                      if(currentItemIndex > lastIndex){
+                          for(let m = lastIndex; m <= currentItemIndex; m++){
+                              items[m].set('isSelected', true);
+                          }
+                      } else {
+                          for(let n = currentItemIndex; n <= lastIndex; n++){
+                              items[n].set('isSelected', true);
+                          }
+                      }
+                  }
+
+              }
+
+          } else {
+              // handle single select
+              selected.clear();
+              selected.pushObject(item);
+
+          }
+
       }
   },
   init () {
