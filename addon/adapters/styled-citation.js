dea7de79d86920fe31e276d993a26298b7889aba
Caner Uguz <caneruguz@gmail.com>
2016-11-21 16:22:36 -0500

    Add working version but hacky elements need to be researched

--- addon/adapters/styled-citation.js
+++ addon/adapters/styled-citation.js
@@ -3,7 +3,9 @@ import OsfAdapter from './osf-adapter';
 export default OsfAdapter.extend({
     buildURL(modelName, id, snapshot, requestType, data) { // jshint ignore:line
         let base = this._super(...arguments).split('styled_', 1);
-        let url = `${base}nodes/h7naf/citation/${id}/`;
+        // Very hacky way to get both node and id in the findRecord call
+        let info = id.split('_');
+        let url = `${base}nodes/${info[1]}/citation/${info[0]}/`;
         console.log(base, url, arguments);
         return url;
     }
--- addon/components/basic-citations/component.js
+++ addon/components/basic-citations/component.js
@@ -4,12 +4,40 @@ import layout from './template';
 export default Ember.Component.extend({
     layout,
     store: Ember.inject.service(),
-    citationTypes : ['apa','modern-language-association','chicago-author-date'],
-    citationTexts : Ember.A(),
+    // Using a very simple method here, may need to advance to Ember.object if other needs arise.
+    citations : Ember.A([
+        Ember.Object.create({
+            id: 'apa',
+            label : 'APA',
+            citation : null,
+            error : null
+        }),
+        Ember.Object.create({
+            id: 'modern-language-association',
+            label : 'MLA',
+            citation : null,
+            error : null
+        }),
+        Ember.Object.create({
+            id: 'chicago-author-date',
+            label : 'Chicago',
+            citation : null,
+            error : null
+        })
+    ]),
     _getStyles () {
-        this.get('store').findRecord('styled-citation', 'apa').then(result => {
-            console.log(result.get('citation'));
-        });
+        for(let c of this.get('citations')){
+            this.get('store').findRecord('styled-citation', c.get('id') + '_h7naf').then(result => {
+                if(result){
+                    c.set('citation', result.get('citation'));
+                } else {
+                    c.set('error', 'Citation couldn\'t load from servers. Try refreshing this page.' );
+                }
+            }, error => {
+                c.set('error', 'Citation couldn\'t load from servers. Try refreshing this page.' );
+            });
+        }
+
     },
     init(){
         this._super(...arguments);
